a1affbf41d412b72306f0d0e6d6b91bac877ea8b
Langen-Tardigrada <46591485+Langen-Tardigrada@users.noreply.github.com>
2020-03-28 02:35:57 +0700

    Updates

---
+++ precache-manifest.07532c8b3375c35e90d054860aa398b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5c7d5ef00456e8664e1f47252ced74",
+    "url": "/expense/index.html"
+  },
+  {
+    "revision": "1dbaac20763d7f28ab39",
+    "url": "/expense/static/css/main.04418a4e.chunk.css"
+  },
+  {
+    "revision": "667f4a4043fb94c5aec8",
+    "url": "/expense/static/js/2.a44ac295.chunk.js"
+  },
+  {
+    "revision": "ad61e5a3703d8af976e070e9896b739a",
+    "url": "/expense/static/js/2.a44ac295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dbaac20763d7f28ab39",
+    "url": "/expense/static/js/main.b370c7e1.chunk.js"
+  },
+  {
+    "revision": "57a64df54f546a461b1b",
+    "url": "/expense/static/js/runtime-main.8f36ef0b.js"
+  }
+]);
